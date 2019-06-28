2ab80321cbcd4e129125e6079ded3d88e44653a1
Grahame Bowland <gbowland@ccg.murdoch.edu.au>
2019-06-28 15:39:31 +0800

    wip on moving to create-react-app

---
+++ frontend/src/setupProxy.js
@@ -0,0 +1,8 @@
+const proxy = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(proxy('/api/', { target: 'http://localhost:8000/' }));
+  app.use(proxy('/admin/', { target: 'http://localhost:8000/' }));
+  app.use(proxy('/static/admin/', { target: 'http://localhost:8000/' }));
+  app.use(proxy('/static/debug_toolbar/', { target: 'http://localhost:8000/' }));
+};
