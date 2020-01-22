a46ec55c87473b15ae9e5af95e856b74862bc4cf
Grahame Bowland <grahame.bowland@qcif.edu.au>
2020-01-22 16:48:18 +0800

    tracking of non-denoised requests in the django backend

--- bpaotu/bpaotu/models.py
+++ bpaotu/bpaotu/models.py
@@ -6,6 +6,14 @@ import logging
 logger = logging.getLogger("rainbow")
 
 
+class NonDenoisedDataRequest(models.Model):
+    match_sequence = models.TextField(null=False)
+    taxonomy_string = models.TextField(null=False)
+    amplicon = models.TextField(null=False)
+    email = models.TextField(null=False)
+    selected_samples = models.TextField(null=False)
+
+
 class ImportMetadata(models.Model):
     methodology = models.CharField(max_length=32)
     revision_date = models.DateField(null=False)
